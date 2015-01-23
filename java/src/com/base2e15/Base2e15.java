af75afeea5be42867f04abf1746607495474266d
Rick Zhou <rinick@gmail.com>
2015-01-23 01:08:00 -0800

    add license comments to the java file

--- java/src/com/base2e15/Base2e15.java
+++ java/src/com/base2e15/Base2e15.java
@@ -1,3 +1,5 @@
+// Copyright (c) 2015, Rick Zhou. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
 package com.base2e15;
 
 import java.util.Arrays;
